20a18c5cb58f67e9aa895a31f4249ad9098d1b15
Richard Bramley <rabramley@gmail.com>
2024-09-05 16:25:30 +0100

    Velocities

---
+++ midi/velocity.py
@@ -0,0 +1,13 @@
+class VelocityPlugin:
+    PYRRUS = [0, 0]
+    IAMB = [0, 1]
+    TROCHEE = [1, 0]
+    SPONDEE = [1, 1]
+    TRIBRACH = [0, 0, 0]
+    DACTYL = [1, 0, 0]
+    AMPHIBRACH = [0, 1, 0]
+    ANAPEST = [0, 0, 1]
+    BACCHIUS = [0, 1, 1]
+    CRETIC = [1, 0, 1]
+    ANTIBACCHIUS = [1, 1, 0]
+    MOLOSSUS = [1, 1, 1]
